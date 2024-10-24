1fb98899d9e502bd2b6636eb5bd946854c7c26b7
Joel Chi <abongsjoel@gmail.com>
2024-10-24 23:16:29 +0100

    Add edit icon to book card

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
+import BookEdit from "./BookEdit";
+
 function BookShow({ book, onDelete }) {
+  const [showEdit, setShowEdit] = useState(false);
+
   return (
     <div className="book-show">
-      {book.title}
+      {showEdit ? <BookEdit /> : <h3>{book.title}</h3>}
       <div className="actions">
+        <button className="edit" onClick={() => setShowEdit(!showEdit)}>
+          Edit
+        </button>
         <button className="delete" onClick={() => onDelete(book.id)}>
           Delete
         </button>
